538dafc32aa319660c4144faf8410d670f307d06
Seshachalam S <seshachalam.s@pwc.com>
2019-02-11 18:44:11 +0530

    first commit

---
+++ precache-manifest.2ff46b96d3cba053ec86f0c6901a91a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc2f671d7fa206bfad16",
+    "url": "/static/css/main.40625b6d.chunk.css"
+  },
+  {
+    "revision": "cc2f671d7fa206bfad16",
+    "url": "/static/js/main.cc2f671d.chunk.js"
+  },
+  {
+    "revision": "aff1a734f244150f626e",
+    "url": "/static/js/1.aff1a734.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5ddeecfba3d5ac497362807121626e42",
+    "url": "/index.html"
+  }
+];
